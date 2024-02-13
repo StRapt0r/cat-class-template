f1324b9073ba5d3b5094fc3aea360a16cf1b41fc
Elijah <87155048+StRapt0r@users.noreply.github.com>
2024-02-13 08:18:15 -0800

    ...

--- cat.py
+++ cat.py
@@ -3,10 +3,16 @@ class Cat():
     def __init__(self):
         self.name="Unknown"
         self.age=0
-    
-def speak(self):
-    print("Meow")
+    def speak(self):
+        return "Meow"
 
+stella = Cat()
+stella.name="Stella"
+stella.age=7
+
+garfield = Cat()
+garfield.age=50
+garfield.name="Garfield"
 
 #Create objects here
 #These should NOT be indented inside the class
